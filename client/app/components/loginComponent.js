5d9a8b686f750681f57a92bd1acd8e1ec57e5b97
Hayden Carlson <haydog2@hotmail.com>
2017-02-03 16:20:24 -0800

    Added login form

--- client/app/components/loginComponent.js
+++ client/app/components/loginComponent.js
@@ -3,8 +3,13 @@ import React, {Component} from 'react';
 class loginBox extends Component {
   render() {
     return (
-    	<div>
-    		<span> Does this even work </span>
+    	<div id="loginForm">
+    		<label> Username </label>
+    		<input type="text" placeholder="Enter Username"/>
+    		<label> Password </label>
+    		<input type="text" placehollder="Enter Password"/>
+    		<label> Company Name </label>
+    		<input type="text" placeholder="Enter Company Name"/>
     	</div>
     );
   };
